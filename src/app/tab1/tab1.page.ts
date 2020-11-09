459e78fa73bf2c44be317a287d6100aa0b070d8e
ramoncarrascom <ramon@ramoncarrasco.es>
2020-11-09 15:52:37 +0100

    ADDED slider components

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,10 +11,7 @@ export class Tab1Page implements OnInit {
 
   peliculasRecientes: Pelicula[] = [];
 
-  slideOpts = {
-    slidesPerView: 1.3,
-    freeMode: true
-  }
+
   constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
